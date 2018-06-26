ae84e500c4584561a416fe1909daa37f71b470ab
Josef Jerabek <josefjerabek@Josefs-MacBook-2.local>
2018-06-26 11:30:33 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,14 @@
+used projects and articles  
+https://github.com/tomyitav/express-es6-starter
+https://medium.com/@jeffandersen/building-a-node-js-rest-api-with-express-46b0901f29b6
+https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
+https://stackoverflow.com/questions/2712878/javascript-regex-pattern-concatenate-with-variable
+
+REST API commands
+/count 
+GET returns number of API calls from server start
+
+/file
+POST with binary file returns "corrected" documentation
+
+best to use it  with postman app
---
+++ server.js
@@ -0,0 +1,46 @@
+﻿const express = require('express');
+const app = express();
+
+let counter = 0;
+
+app.use((req, res, next) => {
+  req.rawBody = '';
+  // req.setEncoding('base64');
+  req.setEncoding(null);
+  req.on('data', function(chunk) {
+    req.rawBody += chunk;
+  });
+  req.on('end', function() {
+    next();
+  });
+});
+
+app.get('/count', (req, res) => res.send('Api calls: '+counter));
+
+app.post('/file', (req, res) => {
+  console.log('Input:');
+  console.log(req.rawBody);
+
+  var fs = require('fs');
+  var array = fs.readFileSync('keywords_example.txt').toString().split("\n"); 
+  let result = req.rawBody;
+  for(i in array) {
+    console.log(array[i]);
+    let re = new RegExp(array[i].trim(),'gi');
+    console.log(re); 
+    result = result.replace(re,array[i]+ " ©" );
+    console.log(result);
+}
+  counter++;
+  console.log('Result:');
+  console.log(result);
+  res.set({"Content-Disposition":"attachment; filename=\"result.txt\""});
+  res.send(result);
+});
+
+var port = process.env.PORT || 3000;
+//app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
+});
